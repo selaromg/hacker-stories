24042cefa1c9a8fae5b1ebc11508c0a6f92e0cc5
Gigi M <selaromg13@gmail.com>
2023-02-21 12:49:10 -0500

    adding and playing with lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as React from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reatjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Ambrov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   const [count, setCount] = useState(0)
