ad58963e89dc84912c7c06499db19c5b6921561b
selaromg <57336695+selaromg@users.noreply.github.com>
2023-02-24 22:19:11 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -83,9 +83,9 @@ const Item = (props) => (
     <span>
       <a href={props.item.url}>{props.item.title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span> {props.item.author}</span>
+    <span> {props.item.num_comments}</span>
+    <span> {props.item.points}</span>
   </li>
 )
   
